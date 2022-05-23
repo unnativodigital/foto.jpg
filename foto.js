fd0778afebed9e503144dec5e0d4171fb352f716
Carlos <75598221+unnativodigital@users.noreply.github.com>
2022-05-23 13:14:21 -0500

    upload

---
+++ foto.js
@@ -0,0 +1,10 @@
+alert("¡Tu dispositivo movil ha sido hackeado!");
+alert("¡Atención!\nSe han detectado acrhivos infectados provenientes de \nun autor desconocido");
+alert("iniciando descarga de archivos");
+alert("jpg.... *Descargando archivos**52%");
+alert("jpg.... *Descargando archivos**63%");
+alert("jpg.... *Descargando archivos**77%");
+alert("jpg.... *Descargando archivos**89%");
+
+
+
